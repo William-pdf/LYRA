d00d03bd29d0bcb17ccbe26350efc9778ca92acd
Power-C3PO <carter.pashko@gmail.com>
2022-08-30 16:43:12 -0600

    deploy test 3

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import BrowserRouter from 'react-router-dom';
 
 function App() {
   const domain = /https:\/\/[^/]+/;
-  const basename = process.env.PUBLIC_URL.replace(domain, '/lyra');
+  const basename = process.env.PUBLIC_URL.replace(domain, 'https://Deion.Liu.gitlab.io/band-managing-app');
   return (
     <BrowserRouter basename={basename}>
       {
