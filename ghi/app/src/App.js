b5d7e495d99625f10c675226c6d1d26d4f3c5ae1
Power-C3PO <carter.pashko@gmail.com>
2022-08-30 16:28:45 -0600

    deployment test 1

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const domain = /https:\/\/[^/]+/;
+  const basename = process.env.PUBLIC_URL.replace(domain, '/lyra');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename={basename}>
+      {
+      <div className="App">
+        <header className="App-header">
+          <p>
+            LYRA TEST PAGE
+          </p>
+        </header>
+      </div>
+      }
+    </BrowserRouter>
   );
 }
 
