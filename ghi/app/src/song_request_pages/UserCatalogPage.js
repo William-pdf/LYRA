bfb7f41a75db5d849d06215da1e3017c6e4ba8d2
Power-C3PO <carter.pashko@gmail.com>
2022-09-13 14:35:29 -0600

    fixed catalog page, all functions are working

--- ghi/app/src/song_request_pages/UserCatalogPage.js
+++ ghi/app/src/song_request_pages/UserCatalogPage.js
@@ -3,95 +3,53 @@ import { useToken } from '../useToken';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+export default function UserCatalog(props) {
 
-
-
-
-
-
-
-
-
-
-export default function UserCatalog() {
-    const [songs, setSongs] = useState()
-    const [token] = useToken();
-    const [user, setUser] = useState();
-    const navigate = useNavigate();
     
+    const [filterSongs, setFilterSongs] = useState([])
+    const [user, setUser] = useState({})
+    let navigate = useNavigate()
+    const [token] = useToken()
     
+
+    useEffect(() => {
+            async function getCurrentUser() {
+                const userUrl = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/tokens/me/`;
+                const userResponse = await fetch(userUrl, {
+                  credentials: 'include',
+                });
+                if (userResponse.ok) {
+                  const userData = await userResponse.json();
+                  setUser(userData);
+                } else {
+                  navigate("/login/")
+                }
+              } 
+            if (token) {
+                getCurrentUser();  
+            } 
+    }, [token])
     
+    useEffect(() => {
+        setFilterSongs(props.songs.filter((song) => {
+            if(song.owner_artist === user.artist_name) {
+                return song;
+            }
+        }))
+    }, [user])
     
-    function navToEdit(songID) {
+    async function navToEdit(songID) {
         navigate(`/catalog/${songID}/`)
         
     }
-    
-    useEffect(() => {
-        async function getCurrentUser() {
-            const url = `${process.env.REACT_APP_ACCOUNTS_HOST}/api/tokens/me/`;
-            const response = await fetch(
-                url, 
-                { credentials: 'include',}
-                );
-                if (response.ok) {
-                    const userData = await response.json();
-                    setUser(userData);  
-                } else {
-                    navigate("/login/")
-                } 
-            } if (token) {
-                getCurrentUser();
-            } 
-            
-            
-    }, [token]);
-        
-    useEffect(() => {
-        async function get_songs() {
-            
-            const url = `${process.env.REACT_APP_DJANGO_SERVICE}/api/songs/`
-            const fetchConfig = {
-                method: "GET",
-                headers: { 'Content-Type': 'application/json' },
-                credentials: "include"
-            }
-            const response = await fetch(url, fetchConfig);
-            if (response.ok) {
-                const songsData = await response.json();
-                setSongs(songsData)
-            }
-            
-        }
-        get_songs();
-    });
 
-    useEffect(() => {
-        function filterSongs() {
-            const filteredSongs = songs.filter(song => {
-                if (song.owner_artist === user.artist_name) {
-                    return song
-                }
-            })
-            setSongs(filteredSongs)
-        }
-        filterSongs();
-    });
-
-        
-        
-        console.log("SONGS:", songs)
-        
-        console.log("USER:", user)
-        console.log("ARTISTNAME:", user)
-        
-    const tBodies = songs.map((song, index) => {
+    const tBodies = filterSongs.map((song, index) => {
         const songValues = Object.values(song)
         const songsRows = songValues.map((value, i) => {
             const songTitle = i === 0 ? <td rowSpan={songValues.length + 1} value={song.id} onClick={() => navToEdit(song.id)}>{song.title}</td> : null
             const songArtist = i === 0 ? <td rowSpan={songValues.length + 1}>{song.artist}</td> : null
             const songRequestable = i === 0 ? <td rowSpan={songValues.length + 1}>{song.is_requestable.toString().toUpperCase()}</td> : null
-            const editLink = i === 0 ? <button rowSpan={songValues.length + 1} value={song.id} onClick={() => navToEdit(song.id)}>EDIT</button> : null
+            const editLink = i === 0 ? <button rowSpan={songValues.length + 1} value={song.id} onClick={() => navigate(`/catalog/${song.id}/`)}>EDIT</button> : null
             return (
                 <tr key={i}>
                     {songTitle}
@@ -131,4 +89,3 @@ export default function UserCatalog() {
         </>
     )
 }
-
